c4d26f132dcbd188f6b0f53668c9e059f3b457e8
Husnain Tariq <34330363+hxnain619@users.noreply.github.com>
2018-11-21 18:52:40 +0500

    Update script.js

--- PageLinking_Assignment/script.js
+++ PageLinking_Assignment/script.js
@@ -1,22 +1,23 @@
-
+window.onload=() => {
+    history.pushState(null,null,'index.html');
+}
 function toChangePath(e) {
-
     var profession = document.getElementById('profession');
     var section = document.getElementById('section');
     var contact = document.getElementById('contact');
     
-if(e.target.getAttribute('class') === "role1"){
-    profession.style.display='block';
-    section.style.display='none';
-    contact.style.display='none';
-}else if(e.target.getAttribute('class') === "contact"){
-    profession.style.display='none';
-    section.style.display='none';
-    contact.style.display='block';
-}else if(e.target.getAttribute('class') === "section"){
-    profession.style.display='none';
-    section.style.display='block';
-    contact.style.display='none';
-}
- 
+    if (e.target.getAttribute('class') === "role1") {
+        profession.style.display = 'block';
+        section.style.display = 'none';
+        contact.style.display = 'none';
+    } else if (e.target.getAttribute('class') === "contact") {
+        profession.style.display = 'none';
+        section.style.display = 'none';
+        contact.style.display = 'block';
+    } else if (e.target.getAttribute('class') === "section") {
+        profession.style.display = 'none';
+        section.style.display = 'block';
+        contact.style.display = 'none';
+    }
+
 }
