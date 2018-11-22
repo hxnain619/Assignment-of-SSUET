d835b320665942e69de7fdac23cbfa170bbbfbaf
Husnain Tariq <34330363+hxnain619@users.noreply.github.com>
2018-11-22 23:40:03 +0500

    Update script.js

--- PageLinking_Assignment/script.js
+++ PageLinking_Assignment/script.js
@@ -1,23 +1,26 @@
 window.onload=() => {
     history.pushState(null,null,'index.html');
+
 }
 function toChangePath(e) {
     var profession = document.getElementById('profession');
     var section = document.getElementById('section');
     var contact = document.getElementById('contact');
-    
+        
     if (e.target.getAttribute('class') === "role1") {
         profession.style.display = 'block';
-        section.style.display = 'none';
-        contact.style.display = 'none';
+        section.style.display =  contact.style.display = 'none';
+        history.pushState(null,null,"#/Profession");
+
     } else if (e.target.getAttribute('class') === "contact") {
-        profession.style.display = 'none';
-        section.style.display = 'none';
+        profession.style.display = section.style.display = 'none';
         contact.style.display = 'block';
+        history.pushState(null,null,"#/Contact");
     } else if (e.target.getAttribute('class') === "section") {
-        profession.style.display = 'none';
-        section.style.display = 'block';
-        contact.style.display = 'none';
+        profession.style.display =  contact.style.display = 'none';
+        section.style.display = 'block';     
+        history.pushState(null,null,"index.html")
     }
 
 }
+
